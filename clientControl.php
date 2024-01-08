2c7b2980d632d9f33c164ade8c7a910918ab5406
Banglin0321 <148021603+Banglin0321@users.noreply.github.com>
2024-01-08 16:32:20 +0800

    顧客MVC

--- clientControl.php
+++ clientControl.php
@@ -1,24 +1,94 @@
 <?php
 require('clientModel.php');
+
 $act=$_REQUEST['act'];
-switch ($act){
-case "listItem":
-  $items=getItemList();
-  echo json_encode($items);
-  return;
-case "addItem":
+switch ($act)
+{
+case 'evaluate':
 	$jsonStr = $_POST['dat'];
-	$item = json_decode($jsonStr);
-	addItem($item->name,$item->price,$item->content,$item->number);
+	$job = json_decode($jsonStr);
+	evaluate($job->id, $job->evaluate);
 	return;
-case "delItem":
-	$id=(int)$_REQUEST['id'];
-	delItem($id);
+case "addOrder":
+	$account=$_REQUEST['account'];
+	addOrder($account);
 	return;
-case "listItem1":
-	$items=getItemList1();
-	echo json_encode($items);
+case "delCart":
+	$account=$_REQUEST['account'];
+	delCart($account);
 	return;
+case "loadOrder":
+	$account=$_REQUEST['account'];
+	$jobs=getOrder($account);
+	echo json_encode($jobs);
+	return;
+case "listJob": // 商品
+  $jobs=getJobList();
+  echo json_encode($jobs);
+  return;
+case "addJob": // 加入購物車
+	$account=$_REQUEST['account'];
+	$jsonStr = $_POST['dat'];
+	$job = json_decode($jsonStr);
+	addJob($job->name,$job->price,$job->content,$job->number,$account,$job->Mid);
+	return;
+case "delJob": // 刪除購物車商品
+	$id=(int)$_REQUEST['id']; //$_GET, $_REQUEST
+	delJob($id);
+	return;
+case "listshopping": // 購物車
+	$account=$_REQUEST['account'];
+	$jobs=getJobList1($account);
+	echo json_encode($jobs);
+	return;
+case "login": // 登入
+	$account=$_REQUEST['account'];
+	$pwd=$_REQUEST['pwd'];
+	$role = login($account,$pwd);
+	setcookie('loginRole',$role);
+	if ($role > 0) {
+		$msg=[
+			"msg" => "OK",
+			"role" => $role
+		];
+	} else {
+		$msg=[
+			"msg" => "NO",
+			"role" => 'none'
+		];
+	}
+	echo json_encode($msg);
+	return;
+case 'showInfo':
+	$loginRole=$_COOKIE['loginRole'];
+	if ($loginRole>0) {
+		$msg="您已經登入,您的角色是 $loginRole. (1.顧客,2.商家,3.物流)";
+	} else {
+		$msg="需要登入去使用這個按鈕";
+	}
+	echo $msg;
+	break;
+case 'logout':
+	setcookie('loginRole',0);
+	break;
+case 'addRole': // 註冊
+	$account=$_REQUEST['account'];
+	$pwd=$_REQUEST['pwd'];
+	$role=(int)$_REQUEST['role'];
+	$condition=addRole($account,$pwd,$role);
+	if ($condition > 0) {
+		$msg=[
+			"msg" => "OK",
+		];
+	} else {
+		$msg=[
+			"msg" => "fail",
+		];
+	}
+	echo json_encode($msg);
+	return ;
+
 default:
+
 }
 ?>
--- clientModel.php
+++ clientModel.php
@@ -1,70 +1,164 @@
 <?php
 require('dbconfig.php');
-
-function getItemList() //賣家清單
+function evaluate($id, $evaluate)
 {
     global $db;
-    $sql = "select * from sell;";
-    $stmt = mysqli_prepare($db, $sql); //建立statement 物件，以便執行SQL
-    mysqli_stmt_execute($stmt); //執行SQL
-    $result = mysqli_stmt_get_result($stmt); //取得查詢結果
-
-    $rows = array(); //要回傳的陣列
-    while ($r = mysqli_fetch_assoc($result)) {
-        $rows[] = $r; //將此筆資料新增到陣列中
-    }
-    return $rows;
+	$sql = 'update `order` set evaluate=? where id=?';
+	$stmt = mysqli_prepare($db, $sql);
+	mysqli_stmt_bind_param($stmt, "si", $evaluate, $id);
+	mysqli_stmt_execute($stmt);
+	return True;
 }
-
-function getItemList1() //買家購物車
+function addOrder($account)
+{
+	global $db;
+	// 先找商家id
+	$sql = "select distinct `Mid` from shopping where uid=?;";
+	$stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $account);
+    mysqli_stmt_execute($stmt);
+	$result = mysqli_stmt_get_result($stmt);
+	$Mid='';
+	while($r = mysqli_fetch_assoc($result))
+	{
+		$Mid= $r['Mid'];
+		$sql = "select `name`,`number`,`total` from shopping where uid=? and Mid=?;";
+		$stmt = mysqli_prepare($db, $sql);
+    	mysqli_stmt_bind_param($stmt, "ss", $account, $Mid);
+    	mysqli_stmt_execute($stmt);
+    	$result2 = mysqli_stmt_get_result($stmt);
+		$commodity = ''; // 商品內容
+		$total=0; // 總金額
+		while($r = mysqli_fetch_assoc($result2))
+		{
+			$commodity .= $r['name'] . ' * ' . $r['number'] .','; //將此筆資料新增到字串中
+			$total+= (int)$r['total'];
+		}
+		$sql = "insert into `order` (commodity, total, userid, Mid) values (?, ?, ?, ?)";
+		$stmt = mysqli_prepare($db, $sql);
+		mysqli_stmt_bind_param($stmt,"siss",$commodity, $total, $account, $Mid);
+		mysqli_stmt_execute($stmt);
+	}
+	return true;
+}
+function delCart($account)
+{
+	global $db;
+	$sql = "delete from shopping where uid=?;";
+	$stmt = mysqli_prepare($db, $sql);
+	mysqli_stmt_bind_param($stmt,"s", $account);
+	mysqli_stmt_execute($stmt);
+	return true;
+}
+function getJobList() // 商品清單
+{
+	global $db;
+	$sql = "select * from shop;";
+	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
+	mysqli_stmt_execute($stmt); //執行SQL
+	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
+	$rows = array(); //要回傳的陣列
+	while($r = mysqli_fetch_assoc($result))
+	{
+		$rows[] = $r; //將此筆資料新增到陣列中
+	}
+	return $rows;
+}
+function getJobList1($account) // 購物車
+{
+	global $db;
+	$sql = "select * from shopping where uid=?;";
+	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
+	mysqli_stmt_bind_param($stmt, "s", $account);
+	mysqli_stmt_execute($stmt); //執行SQL
+	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
+	$rows = array(); //要回傳的陣列
+	while($r = mysqli_fetch_assoc($result))
+	{
+		$rows[] = $r; //將此筆資料新增到陣列中
+	}
+	return $rows;
+}
+function getOrder($account) //訂單
+{
+	global $db;
+	$sql = "select * from `order` where userid=?;";
+	$stmt = mysqli_prepare($db, $sql );
+	mysqli_stmt_bind_param($stmt,"s", $account);
+	mysqli_stmt_execute($stmt);
+	$result = mysqli_stmt_get_result($stmt);
+	$rows = array();
+	while($r = mysqli_fetch_assoc($result))
+	{
+		$rows[] = $r;
+	}
+	return $rows;
+}
+function addJob($name, $price, $content, $number,$account, $Mid) // 加入購物車
 {
     global $db;
-    $sql = "select * from buy;";
-    $stmt = mysqli_prepare($db, $sql); //建立statement 物件，以便執行SQL
-    mysqli_stmt_execute($stmt); //執行SQL
-    $result = mysqli_stmt_get_result($stmt); //取得查詢結果
-
-    $rows = array(); //要回傳的陣列
-    while ($r = mysqli_fetch_assoc($result)) {
-        $rows[] = $r; //將此筆資料新增到陣列中
-    }
-    return $rows;
-}
-
-function addItem($name, $price, $content, $number)
-global $db;
-    $sql = "select number from buy where name=?;";
+    $sql = "select `number` from shopping where name=? and uid=?;";
     $stmt = mysqli_prepare($db, $sql);
-    mysqli_stmt_bind_param($stmt, "s", $name);
+    mysqli_stmt_bind_param($stmt, "ss", $name,$account);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-    if (mysqli_num_rows($result) == 0){
+
+    if (mysqli_num_rows($result) == 0) {
         $total = $number * $price;
-        $sql = "insert into buy (name, price, content, number, total) values (?, ?, ?, ?, ?)";
+        $sql = "insert into shopping (name, price, content, number, total, uid, mid) values (?, ?, ?, ?, ?, ?, ?)";
         $stmt = mysqli_prepare($db, $sql);
-        mysqli_stmt_bind_param($stmt, "sisii", $name, $price, $content, $number, $total);
+        mysqli_stmt_bind_param($stmt, "sisiiss", $name, $price, $content, $number, $total, $account, $Mid);
         mysqli_stmt_execute($stmt);
         return true;
     } else {
         $row = mysqli_fetch_assoc($result);
         $newNumber = $row['number'] + $number;
-  $total = $newNumber * $price;
-        $sql = "update buy set number=?, total=? where name=?";
+		$total = $newNumber * $price;
+        $sql = "update `shopping` set number=?, total=? where name=? and uid=?";
         $stmt = mysqli_prepare($db, $sql);
-        mysqli_stmt_bind_param($stmt, "iis", $newNumber, $total, $name);
+        mysqli_stmt_bind_param($stmt, "iiss", $newNumber, $total, $name, $account);
         mysqli_stmt_execute($stmt);
-        return true;}
+        return true;
+    }
 }
-
-function delItem($id)
+function delJob($id) // 刪除購物車商品
 {
-    global $db;
-
-    $sql = "delete from buy where id=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
-    $stmt = mysqli_prepare($db, $sql); //prepare sql statement
-    mysqli_stmt_bind_param($stmt, "i", $id); //bind parameters with variables, with types "sss":string, string ,string
-    mysqli_stmt_execute($stmt);  //執行SQL
-    return True;
+	global $db;
+	$sql = "delete from shopping where id=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "i", $id); //bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_execute($stmt);  //執行SQL
+	return True;
 }
+function login($account, $pwd) { // 登入
+	global $db;
+	$sql = "select role from users where account=? and password=?;";
+	$stmt = mysqli_prepare($db, $sql );
+	mysqli_stmt_bind_param($stmt, "ss", $account, $pwd);
+	mysqli_stmt_execute($stmt); //執行SQL
+	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
+	if($r = mysqli_fetch_assoc($result)) {
+		return $r['role'];
+	} else {
+		return 0;
+	}
+}
+function addRole($newAccount,$pwd,$role) { // 註冊
+	global $db;
+	$sql = "select `id` from users where account=?";
+    $stmt = mysqli_prepare($db, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $newAccount);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if (mysqli_num_rows($result) == 0) {
+		$sql = "insert into users (account, password, role) values (?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+		$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+		mysqli_stmt_bind_param($stmt, "ssi", $newAccount, $pwd, $role); //bind parameters with variables, with types "sss":string, string ,string
+		mysqli_stmt_execute($stmt);  //執行SQL
+		return 1;
+	} else {
+		return 0;
+	}
 
+}
 ?>
